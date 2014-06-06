1471e2038981e311f0c62c9f8cfe066551ab9195
rsbajracharya <rsbjaracharya@system.deerwalk.com>
2014-06-06 10:13:35 +0545

    Made changes to GenericMethodTest class

--- src/main/java/genericTest/GenericMethodTest.java
+++ src/main/java/genericTest/GenericMethodTest.java
@@ -20,7 +20,7 @@ public class GenericMethodTest
         // Create arrays of Integer, Double and Character
         Integer[] intArray = { 1, 2, 3, 4, 5 };
         Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
-        Character[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+        Character[] charArray = { 'W', 'E', 'L', 'L', 'O' };
 
         System.out.println( "Array integerArray contains:" );
         printArray( intArray  ); // pass an Integer array
